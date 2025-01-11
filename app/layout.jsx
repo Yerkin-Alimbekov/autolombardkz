80827f58d58598e6c0dea6df61ecead467d24e78
Yerkin Alimbekov <98382034+Yerkin-Alimbekov@users.noreply.github.com>
2025-01-11 16:05:08 +0500

    added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter, Bebas_Neue } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`antialiased ${inter.variable} ${bebasNeue.variable}`}>
         {children}
+        <GoogleAnalytics gaId="G-CC3SW4T50C" />
       </body>
     </html>
   );
